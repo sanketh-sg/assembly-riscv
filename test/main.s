e3af5d5cb99da957eee9b0d9dec82ca64df254e2
Sanketh S G <116911914+sanketh-sg@users.noreply.github.com>
2024-06-11 21:43:14 +0000

    Added comments and explanations

--- test/main.s
+++ test/main.s
@@ -1,7 +1,8 @@
 #
 # Copyright 2023 inpyjama.com. All Rights Reserved.
 # Author: Piyush Itankar <piyush@inpyjama.com>
-#
+# PC points to first instruction in the file i,e _start
+# A breakpoint is added to halt the CPU and exe code line by line using GDB
 
 _start:
-  addi x1, x0, 1
+  addi x1, x0, 20
